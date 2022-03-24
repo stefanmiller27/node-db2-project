5f0a66d3d62b06d0d7c12d3333e74836a1b1f29c
Stefan Miller <stefanmiller@Stefans-MacBook-Pro.local>
2022-03-24 05:44:55 -0400

    seed done

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,37 @@
 // STRETCH
+const cars = [ 
+    {
+        vin: '1111111111111',
+        make: 'toyota',
+        model: 'camry',
+        mileage: '205000',
+        title: 'clean',
+        transmission: 'manual'
+    },
+    {
+        vin: '1111111111111',
+        make: 'toyota',
+        model: 'corolla',
+        mileage: '105000',
+        title: 'salvage'
+    },
+    {
+        vin: '1111111111111',
+        make: 'toyota',
+        model: 'supra',
+        mileage: '5000'
+    }
+]
+
+// exports.seed = function(knex) {
+//     return knex('cars')
+//     .truncate()
+//         .then(() => {
+//             return knex('cars').insert(cars)
+//     })
+// }
+
+exports.seed = async function (knex) {
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+}
